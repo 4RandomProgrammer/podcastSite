be0e28e03adb50bffde2c116348bc8bc82d6e6f4
Luís Felipe Henriques <luis.henriques@estudante.ufscar.br>
2024-06-04 22:40:31 -0300

    feat: icons

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Tentativa minha de fazer um site para o podcast se tudo passa o que fica.
 
 
-Através do uso da biblioteca do React e Tailwind CSS
+Através do uso da biblioteca do React e Tailwind CSS. Também utilizando a API do spotify para carregar os iframes.
--- podcast_website/src/App.jsx
+++ podcast_website/src/App.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
 import EpisodeController from './components/EpisodesControler/EpisodeController';
+import { FaInstagram, FaYoutube } from "react-icons/fa";
+import { BsFillTelephoneInboundFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+
 
 function App() {
 
@@ -50,6 +54,21 @@ function App() {
       <footer className='bg-yellow-400'>
         <p>Se tudo passa, o que fica?</p>
         <p>Já se perguntou sobre o verdadeiro sentido da vida? Será que ela é realmente passageira? Somos um grupo de jovens questiionadores que busca as respostas para essas perguntas! Entre nesse bate papo diferente com pessoas que investigam os mistérios da vida e  procuram aprender e se aperfeiçoar. Estes estudantes de logosofia se propõem a extrair o verdadeiro valor das experiências do dia a dia. Afinal, se tudo passa, o que fica?</p>
+        <div>
+        <ul>
+          <li>Redes Sociais</li>
+          <li><FaInstagram /><a href="https://www.instagram.com/setudopassaoquefica/" target="_blank" rel="noopener noreferrer">@setudopassaoqfica</a></li>
+          <li><FaYoutube /><a href="https://www.youtube.com/@setudopassaoquefica2934" target="_blank" rel="noopener noreferrer">setudopassaoquefica</a></li>
+        </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Contato</li>
+            <li><MdEmail />teste@teste.com</li>
+            <li><FaHome />Rua Mundo Transcendente, 1960</li>
+            <li><BsFillTelephoneInboundFill />(11) 11111-1111</li>
+          </ul>
+        </div>
       </footer>
     </div>
   )
